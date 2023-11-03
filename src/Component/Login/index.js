561d5838bc8fccecfaad4b9c99a9bd4caba588b9
abuAbdur-rahman <134824083+abuAbdur-rahman@users.noreply.github.com>
2023-11-03 19:06:13 +0100

    Update index.js

--- src/Component/Login/index.js
+++ src/Component/Login/index.js
@@ -45,7 +45,7 @@ function Login({logins, setLogins, setAproval}) {
     const extraParentStle = {
         marginTop: "100px",
         padding: "100px 0 100px 0",
-        width: "400px",
+        width: "90vw",
         backgroundColor: '#add8e6',
         border: '1px solid black',
         position: 'relative',
